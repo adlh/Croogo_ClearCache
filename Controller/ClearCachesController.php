d9210ddd4e7f279a7a9eba0364c3c766f2ddfcc9
Andrea de la Huerta <info@metamorfosys.de>
2014-04-19 22:30:44 +0200

    Handle permission denied exceptions while unlink

--- Controller/ClearCachesController.php
+++ Controller/ClearCachesController.php
@@ -1,25 +1,39 @@
 <?php
-    /**
-     * ClearCache
-     *
-     * PHP version 5
-     *
-     * @category ClearCache.Controller
-     * @package  Croogo.ClearCache
-     * @version  1.4, 1.5
-     * @author   Lukas Marks <info@lumax-web.de>
-     * @link     https://www.lumax-web.de
-     */
-    App::uses('ClearCachesAppController', 'ClearCache.Controller');
-    
-    class ClearCachesController extends ClearCachesAppController {
-        
-        // use the ClearCache.ClearCache Plugin.Model 
-        public $uses = array('ClearCache.ClearCache');
-        
-        public function admin_clear() {
-                $this->ClearCache->delete();
-                $this->Session->setFlash(__('Cache has been cleared.', true), 'default', array('class' => 'success'));
-                $this->redirect(array('plugin'=>'extensions','controller'=>'extensions_plugins','action'=>'index'));
+/**
+ * ClearCache
+ *
+ * PHP version 5
+ *
+ * @category ClearCache.Controller
+ * @package  Croogo.ClearCache
+ * @version  1.4, 1.5
+ * @author   Lukas Marks <info@lumax-web.de>
+ * @link     https://www.lumax-web.de
+ */
+
+App::uses('ClearCachesAppController', 'ClearCache.Controller');
+
+class ClearCachesController extends ClearCachesAppController {
+
+    // use the ClearCache.ClearCache Plugin.Model
+    public $uses = array('ClearCache.ClearCache');
+
+    public function admin_clear() {
+        $failed = $this->ClearCache->delete();
+        if (empty($failed)) {
+            $this->Session->setFlash(
+                __('Cache has been cleared.', true),
+                'default', array('class' => 'success'));
+        } else {
+            $files = '<br />';
+            foreach ($failed as $f) {
+                $files .= $f . '<br />';
+            }
+            $this->Session->setFlash(
+                __('The following files could not be deleted:', true) .
+                $files, 'default', array('class' => 'error'));
         }
+        $this->redirect(array('plugin'=>'extensions',
+            'controller'=>'extensions_plugins', 'action'=>'index'));
     }
+}
--- Model/ClearCache.php
+++ Model/ClearCache.php
@@ -1,49 +1,56 @@
 <?php
-    /**
-     * ClearCache
-     *
-     * PHP version 5
-     *
-     * @category ClearCache.Model
-     * @package  Croogo.ClearCache
-     * @version  1.4, 1.5
-     * @author   Lukas Marks <info@lumax-web.de>
-     * @link     https://www.lumax-web.de
-     */
-	App::uses('ClearCacheAppModel', 'ClearCache.Model');
+/**
+ * ClearCache
+ *
+ * PHP version 5
+ *
+ * @category ClearCache.Model
+ * @package  Croogo.ClearCache
+ * @version  1.4, 1.5
+ * @author   Lukas Marks <info@lumax-web.de>
+ * @link     https://www.lumax-web.de
+ */
+App::uses('ClearCacheAppModel', 'ClearCache.Model');
 
-    class ClearCache extends ClearCacheAppModel {
+class ClearCache extends ClearCacheAppModel {
 
-        public $name = 'ClearCache';
-        public $useTable = false;
+    public $name = 'ClearCache';
+    public $useTable = false;
 
-        public function delete($path = null, $recursive = true) {
-            
-            if (!$path) { $path = TMP . 'cache' . DS; }
+    public function delete($path = null, $recursive = true, $failed = array()) {
+        if (!$path) {
+            $path = TMP . 'cache' . DS;
+        }
 
-            $dirItems = scandir($path);
-            $ignore = array('.', '..');
+        $dirItems = scandir($path);
+        $ignore = array('.', '..');
+        foreach ($dirItems AS $dirItem) {
+            if (in_array($dirItem, $ignore)) {
+                continue;
+            }
 
-            foreach ($dirItems AS $dirItem) {
-                if (in_array($dirItem, $ignore)) {
-                    continue;
-                }
-
-                if (is_dir($path . $dirItem) && $recursive) {
-                    $this->delete($path . $dirItem . DS);
-                } elseif (substr($dirItem, 0, 5) == 'cake_') {
-                    unlink($path . $dirItem);
-                } elseif (substr($dirItem, 0, 5) == 'type_') {
-                    unlink($path . $dirItem);
-                } elseif (substr($dirItem, 0, 5) == 'node_') {
-                    unlink($path . $dirItem);
-                } elseif (substr($dirItem, 0, 6) == 'nodes_') {
-                    unlink($path . $dirItem);
-                } elseif (substr($dirItem, 0, 7) == 'croogo_') {
-                    unlink($path . $dirItem);
-                } elseif (substr($dirItem, 0, 12) == 'permissions_') {
-                    unlink($path . $dirItem);
+            if (is_dir($path . $dirItem) && $recursive) {
+                $failed = $this->delete($path . $dirItem . DS, true, $failed);
+            } elseif (
+                (substr($dirItem, 0, 5) == 'cake_') ||
+                (substr($dirItem, 0, 5) == 'type_') ||
+                (substr($dirItem, 0, 5) == 'node_') ||
+                (substr($dirItem, 0, 6) == 'nodes_') ||
+                (substr($dirItem, 0, 7) == 'croogo_') ||
+                (substr($dirItem, 0, 12) == 'permissions_')) {
+                    $deleted = false;
+                    try {
+                        $deleted = unlink($path . $dirItem);
+                    } catch (Exception $e) {
+                        $deleted = false;
+                    }
+                    if (!$deleted) {
+                        $failed[] = $dirItem;
+                        //CakeLog::write('debug',
+                            //'Could not delete: ' . $path . $dirItem);
                 }
             }
         }
+        return $failed;
     }
+}
